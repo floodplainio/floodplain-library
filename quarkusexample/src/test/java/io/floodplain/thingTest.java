f982dd16543c1606ef3509b694aac43b2c21d343
Frank Lyaruu <frank@dexels.com>
2020-01-02 14:05:12 +0100

    fix junit, builds now

--- quarkusexample/src/test/java/io/floodplain/thingTest.java
+++ quarkusexample/src/test/java/io/floodplain/thingTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
 public class thingTest {
@@ -15,7 +15,7 @@ public class thingTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(containsString("aap.type"));
     }
 
 }
