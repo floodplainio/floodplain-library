b82f280d515a7aa68bf5f4f479035f47076d72a2
Frank Lyaruu <frank@dexels.com>
2020-02-21 08:05:53 +0100

    shadowJar working now

--- com.dexels.kafka.streams/src/test/java/com/dexels/kafka/streams/processor/programmatic/TestCreateTopology.java
+++ com.dexels.kafka.streams/src/test/java/com/dexels/kafka/streams/processor/programmatic/TestCreateTopology.java
@@ -3,7 +3,6 @@ package com.dexels.kafka.streams.processor.programmatic;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.UUID;
 
 import org.apache.kafka.clients.admin.AdminClient;
@@ -11,11 +10,8 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.streams.Topology;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.dexels.kafka.streams.api.TopologyContext;
-import com.dexels.kafka.streams.base.StreamInstance;
 import com.dexels.kafka.streams.remotejoin.ReplicationTopologyParser;
 import com.dexels.kafka.streams.remotejoin.TopologyConstructor;
 
@@ -24,7 +20,6 @@ public class TestCreateTopology {
 	private static final String BROKERS = "kafka:9092";
 
 	
-	private static final Logger logger = LoggerFactory.getLogger(TestCreateTopology.class);
 
 	@Test
 	public void testTopology() {
@@ -35,7 +30,6 @@ public class TestCreateTopology {
 		Optional<AdminClient> adminClient = Optional.of(AdminClient.create(config));
 		
 		final String applicationId = "shazam-"+UUID.randomUUID().toString();
-		Properties properties = StreamInstance.createProperties( applicationId,BROKERS, "tempdump");
 		System.err.println("ApplicationId: "+applicationId);
 		Topology topology = new Topology();
 		TopologyContext context = new TopologyContext(Optional.of("Generic"), "test", "my_instance", "20191214");
