d68bf5aa6c2f81e3beb6232fa8255ba6e76635f1
Frank Lyaruu <frank@dexels.com>
2020-03-11 14:16:57 +0100

    clean up some more

--- floodplain-function/src/main/java/com/dexels/navajo/functions/XmlEscape.java
+++ floodplain-function/src/main/java/com/dexels/navajo/functions/XmlEscape.java
@@ -4,9 +4,10 @@
  */
 package com.dexels.navajo.functions;
 
-import com.dexels.navajo.document.jaxpimpl.xml.XMLutils;
 import com.dexels.navajo.expression.api.FunctionInterface;
 import com.dexels.navajo.expression.api.TMLExpressionException;
+
+import java.util.StringTokenizer;
 
 /**
  * @author arjen
@@ -42,10 +43,76 @@ public class XmlEscape extends FunctionInterface {
 			return null;
 		}
 		if (o instanceof String) {
-			return XMLutils.XMLEscape((String) o);
+			return XMLEscape((String) o);
 		}  else {
 			throw new TMLExpressionException(this, "Invalid operand: " + o.getClass().getName());
 		}
+	}
+
+
+	/**
+	 * Replace all occurrences of the characters &, ', ", < and > by the escaped
+	 * characters &amp;, &quot;, &apos;, &lt; and &gt;
+	 */
+	private static String XMLEscape(String s) {
+		if ( s == null ) {
+			return "";
+		}
+
+		boolean contains = false;
+		for ( int i = 0; i < XML_ESCAPE_DELIMITERS.length(); i++ ) {
+			if ( s.indexOf( XML_ESCAPE_DELIMITERS.charAt(i) ) != -1 ) {
+				contains = true;
+			}
+		}
+
+		if ( ! contains ) {
+			return s;
+		}
+
+		if (s.length() == 0) {
+			return s;
+		}
+
+		StringTokenizer tokenizer = new StringTokenizer(s, XML_ESCAPE_DELIMITERS, true);
+		StringBuilder    result    = new StringBuilder();
+
+		while (tokenizer.hasMoreElements()) {
+			String substring = tokenizer.nextToken();
+
+			if (substring.length() == 1) {
+				switch (substring.charAt(0)) {
+
+					case '&' :
+						result.append("&amp;");
+						break;
+
+					case ';' :
+						result.append("\\;");
+						break;
+
+					case '<' :
+						result.append("&lt;");
+						break;
+
+					case '>' :
+						result.append("&gt;");
+						break;
+
+					case '\"' :
+						result.append("&quot;");
+						break;
+
+					default :
+						result.append(substring);
+				}
+			}
+			else {
+				result.append(substring);
+			}
+		}
+
+		return result.toString();
 	}
 
 
