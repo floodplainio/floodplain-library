e2b2d0a25ab2d3dbaa55519525d9f2d872415637
Frank Lyaruu <flyaruu@gmail.com>
2023-01-16 15:41:50 +0100

    next snapshot

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -14,7 +14,7 @@ fun isReleaseVersion(): Boolean {
 
 object FloodplainDeps {
     const val kotlin = FloodplainPlugins.kotlin
-    const val floodplain_version = "1.11.3-SNAPSHOT"
+    const val floodplain_version = "1.11.7-SNAPSHOT"
     const val jackson_version = "2.13.3"
     const val kafka_version = "3.1.0"
     const val slf4j_version = "1.7.36"
